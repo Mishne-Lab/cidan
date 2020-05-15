e31332a693e24314ec856e085efaf8e9441aa935
Samuel Schickler <sschickl@ucsd.edu>
2020-05-15 15:43:36 -0700

    added way to view eigen vectors

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='CIDAN',
-    version='0.1.8',
+    version='0.1.9',
     description='CIDAN-Calcium Imaging Data ANalysis',
     license="MIT",
     long_description=long_description,
