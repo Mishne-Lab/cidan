b482b69cc02d7d8fd6c8d6fbc164fdd71623d47a
Samuel Schickler <sschickl@ucsd.edu>
2020-06-02 20:34:42 -0700

    added timetrace calculation code

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "CIDAN/TimeTrace/deltaFOverF.py",
         "CIDAN/TimeTrace/mean.py",
         "CIDAN/LSSC/SpatialBox.py",
+        "CIDAN/TimeTrace/__init__.py",
         "CIDAN/LSSC/functions/roi_filter.py",
         'CIDAN/LSSC/functions/data_manipulation.py',
         "CIDAN/LSSC/functions/eigen.py",
