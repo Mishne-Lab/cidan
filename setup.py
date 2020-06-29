d0cc0e70aebcdffd82e108ef75011aa1c2f997ea
Sam Schickler <gadgetsam@gmail.com>
2020-06-29 08:47:17 -0700

    v 0.1.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='CIDAN',
-    version='0.1.13',
+    version='0.1.14',
     description='CIDAN-Calcium Imaging Data ANalysis',
     license="MIT",
     long_description=long_description,
