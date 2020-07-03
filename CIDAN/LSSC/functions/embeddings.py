96a6474332c8a289f6f47c84d70c85806b71167a
Sam Schickler <gadgetsam@gmail.com>
2020-07-03 11:48:10 -0700

    v 0.1.14

--- CIDAN/LSSC/functions/embeddings.py
+++ CIDAN/LSSC/functions/embeddings.py
@@ -2,11 +2,11 @@ import os
 
 from matplotlib import pyplot as plt
 
-os.environ["OMP_NUM_THREADS"] = "10"  # export OMP_NUM_THREADS=4
-os.environ["OPENBLAS_NUM_THREADS"] = "10"  # export OPENBLAS_NUM_THREADS=4
-os.environ["MKL_NUM_THREADS"] = "10"  # export MKL_NUM_THREADS=6
-os.environ["VECLIB_MAXIMUM_THREADS"] = "10"  # export VECLIB_MAXIMUM_THREADS=4
-os.environ["NUMEXPR_NUM_THREADS"] = "10"  # export NUMEXPR_NUM_THREADS=6
+# os.environ["OMP_NUM_THREADS"] = "10"  # export OMP_NUM_THREADS=4
+# os.environ["OPENBLAS_NUM_THREADS"] = "10"  # export OPENBLAS_NUM_THREADS=4
+# os.environ["MKL_NUM_THREADS"] = "10"  # export MKL_NUM_THREADS=6
+# os.environ["VECLIB_MAXIMUM_THREADS"] = "10"  # export VECLIB_MAXIMUM_THREADS=4
+# os.environ["NUMEXPR_NUM_THREADS"] = "10"  # export NUMEXPR_NUM_THREADS=6
 import scipy.sparse as sparse
 import hnswlib as hnsw
 import numpy as np
